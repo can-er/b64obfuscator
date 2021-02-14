00a7b917a4b765e02827046ccfe91a5aa4675f99
can-er <47574594+can-er@users.noreply.github.com>
2021-02-14 14:17:10 +0100

    Add files via upload

---
+++ b64obfuscator.py
@@ -0,0 +1,22 @@
+"""
+Author: GorillaGlue
+pip3 install -r requirements.txt
+Usage: python3 b64obfuscer.py path/of/source.py path/of/destination.py 
+Note: source & destination must be .py file. Destination is created by the tool itself
+"""
+
+import base64
+import os
+import sys
+
+cwd = os.getcwd()
+path_in = sys.argv[1]
+
+
+with open(path_in, "r", encoding='UTF-8') as content:
+    code = base64.b64encode(bytes(content.read(), 'utf-8'))
+    path_out = sys.argv[2]
+    with open(path_out, "w") as content2:
+        payload = "import base64 \nexec(base64.b64decode({}))".format(code)
+        print(payload)
+        content2.write(payload)
